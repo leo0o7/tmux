465bb5195367f7098f20a22f26e5c7da58dab318
leo0o7 <giardinoleonardo34@gmail.com>
2025-09-16 10:59:20 +0200

    catpuccin

--- weather.sh
+++ weather.sh
@@ -5,8 +5,8 @@ LOCATION=$(curl --silent http://ip-api.com/csv)
 CITY=$(echo "$LOCATION" | cut -d , -f 6)
 
 if [ -n "$CITY" ]; then
-	RES=$(curl -s "wttr.in/$CITY?format="%c+%t+%h+%p)
-	echo "$RES"
+  RES=$(curl -s "wttr.in/$CITY?format="%c+%t+%h+%p)
+  echo "$RES"
 else
-	echo "No info available"
+  echo "No info available"
 fi
